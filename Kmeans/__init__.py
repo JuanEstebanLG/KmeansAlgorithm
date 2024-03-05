2b836dff5fc65c648849c8840d66be02dab81b84
JuanEstebanLG <56986294+JuanEstebanLG@users.noreply.github.com>
2024-03-04 19:42:47 -0500

    Update __init__.py
    Update the main file, add comments

--- Kmeans/__init__.py
+++ Kmeans/__init__.py
@@ -1,12 +1,16 @@
+'''
+ Author: JuanEstebanLG
+'''
 
-import numpy as np
-import matplotlib.pyplot as plt
+'Importamos las librerias necesarios'
+import numpy as np 'numpy nos ayudara con la matematica y operaciones entre arrays del proyecto'
+import matplotlib.pyplot as plt 'matplotlib nos ayudara a graficar los puntos y los centroids'
 
-
+'Algoritmo para R1, con k = 2'
 def kmeanR1():
     plt.style.use('dark_background')
 
-    # Make data
+    # Inicializamos las variables
 
     x1 = np.random.standard_normal(50) * 0.6 + np.ones(50)
 
@@ -15,7 +19,7 @@ def kmeanR1():
     X = np.concatenate((x1, x2), axis=0)
 
 
-
+    #El numero de clusters representara la cantidad de grupos de datos
     num_clusters = 2
 
     # Inicializar los centroides de forma aleatoria
@@ -30,23 +34,27 @@ def kmeanR1():
     old_centroids = np.zeros_like(centroids)
 
     for _ in range(max_iterations):
-
+        
+        #Calculamos las distancias en cada iteracion como la el valor absoluto de la diferencia entre los datos en X y los centroides
         distances = np.abs(X[:, np.newaxis] - centroids)
 
+        #Asignamos los clusters como el punto con menor distancia
         clusters = np.argmin(distances, axis=1)
 
         # Actualizar los centroides
         old_centroids = centroids.copy()
 
         for i in range(num_clusters):
+            #Se re asigna cada centroid como la media de su respectivo cluster
             centroids[i] = np.mean(X[clusters == i])
 
+            #Grafica
             plt.plot(X, np.zeros_like(X), 'w.', markersize=8, label='dataPoints')
             plt.plot(centroids, np.zeros_like(centroids), 'rx', markersize=17, label='Centroides')
             plt.legend()
             plt.show()
 
-            # Verificar la convergencia
+            # Verificar la convergencia, para este caso, si el cambio en los centroids es menor o igual a 0.0001 se rompe el ciclo
 
         if np.mean(np.abs(centroids - old_centroids)) <= tolerance:
             break
@@ -63,6 +71,8 @@ def kmeanR2():
     X = np.concatenate((x1, x2), axis=0)
 
     num_clusters = 2
+
+    #Se asigna de forma al azar cada centroid 
     centroids = X[np.random.choice(len(X), num_clusters, replace=False)]
     max_range = 150
     tolerance = 1e-4
@@ -70,13 +80,19 @@ def kmeanR2():
     old_centroids = np.zeros_like(X)
 
     for _ in range(max_range):
+        #Se calcula la distancia utilizando la distancia euclidiana entre vectores
         distances = np.linalg.norm(X[:, np.newaxis, :] - centroids, axis=2)
 
+        #Se asigna cada cluster como el punto con distancia menor en el eje Y
         clusters = np.argmin(distances, axis=1)
 
+        #Se guarda una copia de los centroides 
         old_centroids = np.copy(centroids)
         for i in range(num_clusters):
+            #Se reasignan los centroides como la media de los puntos en su cluster a lo largo del eje X
             centroids[i] = X[clusters == i].mean(axis=0)
+
+            #Grafica
             plt.plot(X[:, 0], X[:, 1], 'w.', label='Puntos')
             plt.plot(centroids[:, 0], centroids[:, 1], 'rx', markersize=10, label='Centroides')
             plt.legend()
